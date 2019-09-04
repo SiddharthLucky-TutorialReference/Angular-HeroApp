0763e4a835484566f9caae78926503977a51b637
=SiddharthLucky <=siddharth.luckyreddy@gmail.com>
2019-09-04 19:45:43 -0400

    Added Hero's component, uses for loops and on click listeners.

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
+import { Heros } from '../mock-hero';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+};
+
+  /*Since Heros is a value and not an object*/
+  heros = Heros;
+
+  selectedHero: Hero;
+  onSelectHero(hero: Hero) {
+    this.selectedHero = hero;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
